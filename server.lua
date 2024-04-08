b25993f887de8c88ed41b7bc2c567f4bffcc348c
iWN <111748644+iWN3@users.noreply.github.com>
2024-04-08 17:22:32 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,18 +1,18 @@
 local Cooldown = false
 local Duration = 0
 
-lib.callback.register("projectx-utility:server:UtilityCooldown", function(source)
+lib.callback.register("projectx-robberyutility:server:UtilityCooldown", function(source)
     return Duration == 0 or (os.time() - Duration) > 0
 end)
 
-lib.callback.register("projectx-utility:server:CheckOnCooldown", function(source)
+lib.callback.register("projectx-robberyutility:server:CheckOnCooldown", function(source)
     return Cooldown
 end)
 
-RegisterNetEvent("projectx-utility:server:ActivateUtilityCooldown", function(bool)
+RegisterNetEvent("projectx-robberyutility:server:ActivateUtilityCooldown", function(bool)
     if bool then Duration = os.time() + (Config.Duration * 60) else Duration = 0 end
 end)
 
-RegisterNetEvent("projectx-utility:server:OnCooldown", function(bool)
+RegisterNetEvent("projectx-robberyutility:server:OnCooldown", function(bool)
 	Cooldown = bool
 end)
